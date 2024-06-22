a728300ab4d928a08de392a02a86feb7548e7a83
Your Name <your_email@example.com>
2024-06-22 06:30:08 +0530

    native

--- App.tsx
+++ App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Alert,
+  Button,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
+import Company from './Components/Company';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
+  // const name = "harash"
+  const harash=()=>{
+    console.warn("Function Called")
+    
+  }
+  const [name, setname] = useState("")
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      {/* <Text style={{ fontSize: 40, marginTop: 90 }}>React  Native with TS</Text>
+    <Button title='Press here'></Button>
+    <Text>Harash poriya</Text>
+    <Text>{name}</Text>
+    <Button title='Press me'></Button>
+    <Company/> */}
+
+    {/* <Text style={{ fontSize: 30}}>Button and onPress Event</Text>
+
+    <Button title='On Press' onPress={()=>harash()} color={"green"}/> */}
+
+    {/* <Text style={{fontSize:30,color:"red", backgroundColor:"black", marginBottom:10}}>Style in React Native</Text>
+    <Text style={styles.textBox}>Style in React Native</Text>
+    <Text style={styles.textBox}>Style in React Native</Text>
+    <Text style={styles.textBox}>Style in React Native</Text> */}
+   
+   <Text style={{fontSize:30}}>Handle Text Input</Text>
+   <Text style={{fontSize:30}}>Your name is:{name}</Text>
+   <TextInput
+   style={styles.textInput}
+    placeholder='Enter your name' 
+    value={name}
+    onChangeText={(text)=>setname(text)}/>
+    <Button title='Clear input value' onPress={()=>setname("")} />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
+// const styles=StyleSheet.create({
+//   textBox:{
+//     color:"blue",
+//     fontSize:30,
+//     backgroundColor:"black",
+//     marginBottom:20,  
+//     padding:20,
+//     borderRadius:20,
+//     height:100,
+//     textAlignVertical:"center",
+//     textAlign:"center",
+//     borderColor:"red",
+//     borderWidth:5,
+//   }
+// })
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  textInput:{
+    fontSize:20,
+    color:"red", 
+    borderColor:"blue",
+     borderWidth:2
+  }
+})
+
 
 export default App;
---
+++ Components/Company.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from "react-native"
+function Company() {
+  return (
+    <View>
+      <Text>Name:XYZ</Text>
+      <Text>Product: clothes</Text>
+      <Text>Owner Name: harash poriya</Text>
+    </View>
+  )
+}
+
+export default Company
